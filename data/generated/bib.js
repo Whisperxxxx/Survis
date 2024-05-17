631f64ad5272d1f53c3450ae5994e4323ceffa4a
Whisper <silenceplz18@gmail.com>
2024-05-17 07:31:00 +0100

    Survis

---
+++ data/generated/bib.js
@@ -0,0 +1,94 @@
+﻿const generatedBibEntries = {
+    "alexander2013investigation": {
+        "author": "Alexander, Justin T and Sear, John and Oikonomou, Andreas",
+        "journal": "Entertainment computing",
+        "number": "1",
+        "pages": "53--62",
+        "publisher": "Elsevier",
+        "title": "An investigation of the effects of game difficulty on player enjoyment",
+        "type": "article",
+        "volume": "4",
+        "year": "2013"
+    },
+    "chanel2008boredom": {
+        "author": "Chanel, Guillaume and Rebetez, Cyril and B{\\'e}trancourt, Mireille and Pun, Thierry",
+        "booktitle": "Proceedings of the 12th international conference on Entertainment and media in the ubiquitous era",
+        "pages": "13--17",
+        "title": "Boredom, engagement and anxiety as indicators for adaptation to difficulty in games",
+        "type": "inproceedings",
+        "year": "2008"
+    },
+    "colwell2018colwell": {
+        "author": "Colwell, Anthony M and Glavin, Frank G",
+        "journal": "arXiv preprint arXiv:1806.04471",
+        "title": "Colwell's Castle Defence: A Custom Game Using Dynamic Difficulty Adjustment to Increase Player Enjoyment",
+        "type": "article",
+        "year": "2018"
+    },
+    "constant2019dynamic": {
+        "author": "Constant, Thomas and Levieux, Guillaume",
+        "booktitle": "Proceedings of the 2019 CHI conference on human factors in computing systems",
+        "pages": "1--12",
+        "title": "Dynamic difficulty adjustment impact on players' confidence",
+        "type": "inproceedings",
+        "year": "2019"
+    },
+    "denisova2015adaptation": {
+        "author": "Denisova, Alena and Cairns, Paul",
+        "booktitle": "Proceedings of the 2015 Annual Symposium on Computer-Human Interaction in Play",
+        "pages": "97--101",
+        "title": "Adaptation in digital games: the effect of challenge adjustment on player performance and experience",
+        "type": "inproceedings",
+        "year": "2015"
+    },
+    "dziedzic2018approaches": {
+        "author": "Dziedzic, Dagmara and W{\\l}odarczyk, Wojciech",
+        "journal": "International Journal of Human--Computer Interaction",
+        "number": "8",
+        "pages": "707--715",
+        "publisher": "Taylor \\& Francis",
+        "title": "Approaches to measuring the difficulty of games in dynamic difficulty adjustment systems",
+        "type": "article",
+        "volume": "34",
+        "year": "2018"
+    },
+    "moon2020dynamic": {
+        "author": "Moon, Hee-Seung and Seo, Jiwon",
+        "booktitle": "Adjunct Proceedings of the 33rd Annual ACM Symposium on User Interface Software and Technology",
+        "pages": "13--15",
+        "title": "Dynamic difficulty adjustment via fast user adaptation",
+        "type": "inproceedings",
+        "year": "2020"
+    },
+    "paraschos2023game": {
+        "author": "Paraschos, Panagiotis D and Koulouriotis, Dimitrios E",
+        "journal": "International Journal of Human--Computer Interaction",
+        "number": "1",
+        "pages": "1--22",
+        "publisher": "Taylor \\& Francis",
+        "title": "Game difficulty adaptation and experience personalization: A literature review",
+        "type": "article",
+        "volume": "39",
+        "year": "2023"
+    },
+    "qin2010effects": {
+        "author": "Qin, Hua and Rau, Pei-Luen Patrick and Salvendy, Gavriel",
+        "journal": "Interacting with computers",
+        "number": "3",
+        "pages": "230--239",
+        "publisher": "Oxford University Press Oxford, UK",
+        "title": "Effects of different scenarios of game difficulty on player immersion",
+        "type": "article",
+        "volume": "22",
+        "year": "2010"
+    },
+    "zohaib2018dynamic": {
+        "author": "Zohaib, Mohammad and others",
+        "journal": "Advances in Human-Computer Interaction",
+        "publisher": "Hindawi",
+        "title": "Dynamic difficulty adjustment (DDA) in computer games: A review",
+        "type": "article",
+        "volume": "2018",
+        "year": "2018"
+    }
+};
